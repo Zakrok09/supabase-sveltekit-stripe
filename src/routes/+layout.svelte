5b231f5fea120b29504bd9fcae7fdeec6348372d
Zakrok09 <eccyboo@protonmail.com>
2023-05-20 02:11:56 +0200

    Setup skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-  import "../app.postcss";
+    import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
+    import '../app.postcss';
+
 </script>
 
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config}*/
-const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    require('path').join(require.resolve(
+            '@skeletonlabs/skeleton'),
+        '../**/*.{html,js,svelte,ts}'
+    )
+  ],
   theme: {
     extend: {},
   },
-
-  plugins: [],
-};
-
-module.exports = config;
+  plugins: [
+    ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+  ]
+}
